ecfd30cbca490ec3c26efe519baeb05846b5634a
DiazRock <corolariodiaz@gmail.com>
2024-03-05 23:26:01 +0200

    More changes

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from build_index import build_index
+from build_index import build_index, add_message, reply
+from faiss import IndexFlatL2
+
 
 st.title("ChatGPT-like clone")
 
@@ -20,6 +22,35 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
+
+if st.sidebar.button("Reset conversasion"):
+    st.session_state.messages = []
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["agent"]):
+        st.write(message["content"])
+
+if not "text" in st.session_state:
+    add_message(
+        """
+            This is a simple demonstration of how to use large language model and a vector database
+            to implement a bare-bones chat-with-your-pdf application
+
+        """,
+        store=False
+    )
+
+    add_message(
+        "To begin, please upload your PDF file in the sidebar",
+        store=False
+    )
+
+
 pdf = st.sidebar.file_uploader(
     "Upload a PDF file",
     type="PDF",
@@ -27,3 +58,22 @@ pdf = st.sidebar.file_uploader(
     on_change=build_index
 )
 
+if not pdf:
+    st.stop()
+
+
+index: IndexFlatL2 = st.session_state.index
+query = st.chat_input("Ask something about your PDF")
+
+
+if not st.session_state.messages:
+    reply(
+        "In one sentence, what is this document about?",
+        index,
+    )
+    add_message("Ready to answer your questions.")
+
+
+if query:
+    add_message(query, agent="human", stream=False, store=True)
+    reply(query, index)
